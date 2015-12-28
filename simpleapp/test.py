4a747cc542bd5a87a8ab614c762b052869fc5da8
Zack Chase <zack@distelli.com>
2015-12-28 15:55:56 -0800

    changed test to fail

--- simpleapp/test.py
+++ simpleapp/test.py
@@ -8,7 +8,7 @@ class PrimesTestCase(unittest.TestCase):
 
     def test_is_output_hw(self):
         """Is the output of your Python Application what you expect?"""
-        self.assertTrue(hello_world() == "<h1>Hello World from an updated and redeployed Python Application!</h1>")
+        self.assertTrue(hello_world() == "<h1>ello World from an updated and redeployed Python Application!</h1>")
 
 if __name__ == '__main__':
     unittest.main()
